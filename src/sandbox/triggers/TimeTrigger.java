2e3a2a61d1d235a98a137c9cf2869babe752710b
Jason Cameron <dev.jasoncameron@gmail.com>
2024-08-02 23:19:19 -0500

    moving triggers from the check mod to their own class/obj

---
+++ src/sandbox/triggers/TimeTrigger.java
@@ -0,0 +1,45 @@
+package sandbox.triggers;
+
+public class TimeTrigger {
+    private long time;
+    private long interval;
+    private boolean triggered;
+
+    // CON
+
+    // GETs & SETs
+
+    public long getTime() {
+        return time;
+    }
+    public void setTime(long time) {
+        this.time = time;
+    }
+
+    public long getInterval() {
+        return interval;
+    }
+    public void setInterval(long interval) {
+        this.interval = interval;
+    }
+
+    public boolean isTriggered() {
+        return triggered;
+    }
+    public void setTriggered(boolean triggered) {
+        this.triggered = triggered;
+    }
+
+
+    // METHs
+
+    // OVRs
+    @Override
+    public String toString() {
+        return "TimeTrigger{" +
+                "time=" + time +
+                ", interval=" + interval +
+                ", triggered=" + triggered +
+                '}';
+    }
+}
