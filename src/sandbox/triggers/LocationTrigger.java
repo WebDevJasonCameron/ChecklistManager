218fcde0e5c84a2ea474b7709003079d2cfbd76e
Jason Cameron <dev.jasoncameron@gmail.com>
2024-08-05 21:23:08 -0500

    still fig out the trig

---
+++ src/sandbox/triggers/LocationTrigger.java
@@ -0,0 +1,8 @@
+package sandbox.triggers;
+
+public class LocationTrigger {
+    private String location;
+    private long radius;
+    private boolean triggered;
+    private boolean completed;
+}
