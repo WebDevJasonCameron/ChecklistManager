f318a8e2afa6124357d897bef817aea9b79b84f6
Jason Cameron <dev.jasoncameron@gmail.com>
2024-08-08 21:23:35 -0500

    g & s

--- src/sandbox/triggers/LocationTrigger.java
+++ src/sandbox/triggers/LocationTrigger.java
@@ -5,4 +5,50 @@ public class LocationTrigger {
     private long radius;
     private boolean triggered;
     private boolean completed;
+
+    // CON
+
+    // GETs & SETS
+
+    public String getLocation() {
+        return location;
+    }
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public long getRadius() {
+        return radius;
+    }
+    public void setRadius(long radius) {
+        this.radius = radius;
+    }
+
+    public boolean isTriggered() {
+        return triggered;
+    }
+    public void setTriggered(boolean triggered) {
+        this.triggered = triggered;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+
+    // METHs
+
+    // OVRs
+    @Override
+    public String toString() {
+        return "LocationTrigger{" +
+                "location='" + location + '\'' +
+                ", radius=" + radius +
+                ", triggered=" + triggered +
+                ", completed=" + completed +
+                '}';
+    }
 }
